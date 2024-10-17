1e635b3cbe243d1b162fadc3c6943f0bcafc4a90
k6sun1 <k6sun@uwaterloo.ca>
2024-10-17 10:42:58 -0400

    my work progress

--- src/components/home.js
+++ src/components/home.js
@@ -10,14 +10,16 @@ function Home() {
   const matchaSpill = process.env.PUBLIC_URL + "images/matchabottom.png";
   const subbudImg = process.env.PUBLIC_URL + "images/subbud-ver2.png";
   const clearSpider = process.env.PUBLIC_URL + "images/clearspiderBarcode.png";
+  const moodCraft = process.env.PUBLIC_URL + "images/moodcraft.png";
+  const artwork = process.env.PUBLIC_URL + "images/artwork.png";
 
   // const projectsBackground =
   //   process.env.PUBLIC_URL + "";
 
   return (
     <div>
+      {" "}
       <Navigation className="navhome" />
-
       <div className="home">
         <img src={background} alt="background" />
         <h1>Katie Sun</h1>
@@ -42,13 +44,45 @@ function Home() {
       <div className="matcha-spill">
         <img src={matchaSpill} alt="bottom of matcha" />
       </div>
-      <div className="Subbud">
-      <img src={subbudImg} alt="subbud" />
-      <h1>Streamlining subscription management with SubBud.</h1>
+      <div className="projectsSection">
+        <div className="Subbud">
+          <img src={subbudImg} alt="subbud" />
+          <h1>Streamlining subscription management with SubBud.</h1>
+        </div>
+        <div className="clearSpider">
+          <img src={clearSpider} alt="clear spider redesign" />
+          <h1>
+            Redesigning Clear Spider feature landing page for enhanced visual
+            engagement.
+          </h1>
+        </div>
+        <div className="moodCraft">
+          <img src={moodCraft} alt="moodcraft" />
+          <h1>
+            Designing to promote emotional well-being through creative
+            expressions
+          </h1>
+        </div>
       </div>
-      <div className="clearSpider">
-      <img src={clearSpider} alt="clear spider redesign" />
-      <h1>Redesigning feature landing page for enhanced visual engagement.</h1>
+      <div className="moreProjects">
+        <div className="moreProjectsText">
+          <h1>Other projects I've worked on!</h1>
+        </div>
+
+        <div className="moodCraft">
+          <img src={moodCraft} alt="moodcraft" />
+          <h1>
+            Designing to promote emotional well-being through creative
+            expressions
+          </h1>
+        </div>
+        <div className="moodCraft">
+          <img src={artwork} alt="my artwork" />
+          <h1>
+            Designing to promote emotional well-being through creative
+            expressions
+          </h1>
+        </div>
       </div>
     </div>
   );
