ed1794428d17c11920acab1e4b37b29ba10bfd18
k6sun1 <k6sun@uwaterloo.ca>
2024-12-06 10:15:26 -0500

    updated resume link

--- src/components/home.js
+++ src/components/home.js
@@ -75,7 +75,7 @@ function Home() {
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://drive.google.com/file/d/1LYqSUE5yBAUkqegN05GTDuTkBSvlGPNj/view?usp=sharing"
+            href="https://drive.google.com/file/d/1wE5oW-t4HMkUHFS9yIIoC1koD2aKcRzA/view?usp=sharing"
           >
           <div className="intro-button">
             <button>View Resume</button>
